c78eae3a96e147c46dc23c718c28bb98e6a21fa0
huangkaifen <huangkaifen@lizhi.fm>
2024-10-18 21:50:32 +0800

    feat: 实现两个角色对话、保存对话数据、根据风格生成图片

--- characterglm_api_demo_streamlit.py
+++ characterglm_api_demo_streamlit.py
@@ -54,10 +54,10 @@ MsgList = List[Msg]
 
 
 class CharacterMeta(TypedDict):
-    user_info: str  # 用户人设
-    bot_info: str   # 角色人设
-    bot_name: str   # bot扮演的角色的名字
-    user_name: str  # 用户的名字
+    role_A_name: str   # 角色A名字
+    role_A_info: str   # 角色A人设
+    role_B_name: str  # 角色B名字
+    role_B_info: str  # 角色B人设
 
 
 def filter_text_msg(messages: MsgList) -> TextMsgList:
@@ -189,22 +189,22 @@ def generate_role_appearance(role_profile: str):
 def generate_chat_scene_prompt(messages: TextMsgList, meta: CharacterMeta):
     """ 调用chatglm生成cogview的prompt，描写对话场景 """
     instruction = f"""
-阅读下面的角色人设与对话，生成一段文字描写场景。
+阅读下面的角色A人设与对话，生成一段文字描写场景。
 
-{meta['bot_name']}的人设：
-{meta['bot_info']}
+{meta['role_A_name']}的人设：
+{meta['role_A_info']}
     """.strip()
-    
-    if meta["user_info"]:
+
+    if meta["role_B_info"]:
         instruction += f"""
 
-{meta["user_name"]}的人设：
-{meta["user_info"]}
+{meta["role_B_name"]}的人设：
+{meta["role_B_info"]}
 """.rstrip()
 
     if messages:
-        instruction += "\n\n对话：" + '\n'.join((meta['bot_name'] if msg['role'] == "assistant" else meta['user_name']) + '：' + msg['content'].strip() for msg in messages)
-    
+        instruction += "\n\n对话：" + '\n'.join((meta['role_A_name'] if msg['role'] == "assistant" else meta['role_B_name']) + '：' + msg['content'].strip() for msg in messages)
+
     instruction += """
     
 要求如下：
@@ -252,6 +252,9 @@ debug = os.getenv("DEBUG", "").lower() in ("1", "yes", "y", "true", "t", "on")
 
 def update_api_key(key: Optional[str] = None):
     global API_KEY
+    # 检查并初始化 API_KEY
+    if 'API_KEY' not in st.session_state:
+        st.session_state['API_KEY'] = API_KEY  # 或者设置为 null
     if debug:
         print(f'update_api_key. st.session_state["API_KEY"] = {st.session_state["API_KEY"]}, key = {key}')
     key = key or st.session_state["API_KEY"]
@@ -267,10 +270,10 @@ if "history" not in st.session_state:
     st.session_state["history"] = []
 if "meta" not in st.session_state:
     st.session_state["meta"] = {
-        "user_info": "",
-        "bot_info": "",
-        "bot_name": "",
-        "user_name": ""
+        "role_A_name": "",
+        "role_A_info": "",
+        "role_B_name": "",
+        "role_B_info": "",
     }
 
 
@@ -280,28 +283,28 @@ def init_session():
 
 # 4个输入框，设置meta的4个字段
 meta_labels = {
-    "bot_name": "角色名",
-    "user_name": "用户名", 
-    "bot_info": "角色人设",
-    "user_info": "用户人设"
+    "role_A_name": "角色A名",
+    "role_A_info": "角色A人设",
+    "role_B_name": "角色B名",
+    "role_B_info": "角色B人设"
 }
 
 # 2x2 layout
 with st.container():
     col1, col2 = st.columns(2)
     with col1:
-        st.text_input(label="角色名", key="bot_name", on_change=lambda : st.session_state["meta"].update(bot_name=st.session_state["bot_name"]), help="模型所扮演的角色的名字，不可以为空")
-        st.text_area(label="角色人设", key="bot_info", on_change=lambda : st.session_state["meta"].update(bot_info=st.session_state["bot_info"]), help="角色的详细人设信息，不可以为空")
-        
+        st.text_input(label="角色A名", key="role_A_name", on_change=lambda : st.session_state["meta"].update(role_A_name=st.session_state["role_A_name"]), help="模型所扮演的角色的名字，不可以为空")
+        st.text_area(label="角色A人设", key="role_A_info", on_change=lambda : st.session_state["meta"].update(role_A_info=st.session_state["role_A_info"]), help="角色的详细人设信息，不可以为空")
+
     with col2:
-        st.text_input(label="用户名", value="用户", key="user_name", on_change=lambda : st.session_state["meta"].update(user_name=st.session_state["user_name"]), help="用户的名字，默认为用户")
-        st.text_area(label="用户人设", value="", key="user_info", on_change=lambda : st.session_state["meta"].update(user_info=st.session_state["user_info"]), help="用户的详细人设信息，可以为空")
+        st.text_input(label="角色B名", value="用户", key="role_B_name", on_change=lambda : st.session_state["meta"].update(role_B_name=st.session_state["role_B_name"]), help="用户的名字，不可以为空")
+        st.text_area(label="角色B人设", value="", key="role_B_info", on_change=lambda : st.session_state["meta"].update(role_B_info=st.session_state["role_B_info"]), help="用户的详细人设信息，不可以为空")
 
 
 def verify_meta() -> bool:
-    # 检查`角色名`和`角色人设`是否空，若为空，则弹出提醒
-    if st.session_state["meta"]["bot_name"] == "" or st.session_state["meta"]["bot_info"] == "":
-        st.error("角色名和角色人设不能为空")
+    # 检查`角色A名`和`角色A人设`是否空，若为空，则弹出提醒
+    if st.session_state["meta"]["role_A_name"] == "" or st.session_state["meta"]["role_A_info"] == "":
+        st.error("角色A名和角色A人设不能为空")
         return False
     else:
         return True
@@ -320,15 +323,15 @@ def draw_new_image():
         )
         
     else:
-        image_prompt = "".join(generate_role_appearance(st.session_state["meta"]["bot_info"]))
-    
+        image_prompt = "".join(generate_role_appearance(st.session_state["meta"]["role_A_info"]))
+
     if not image_prompt:
         st.error("调用chatglm生成Cogview prompt出错")
         return
     
     # TODO: 加上风格选项
-    image_prompt = '二次元风格。' + image_prompt.strip()
-    
+    image_prompt = st.session_state["picture_style"] + '风格。' + image_prompt.strip()
+
     print(f"image_prompt = {image_prompt}")
     n_retry = 3
     st.markdown("正在生成图片，请稍等...")
@@ -355,6 +358,7 @@ def draw_new_image():
 button_labels = {
     "clear_meta": "清空人设",
     "clear_history": "清空对话历史",
+    "select_style": "切换风格:",
     "gen_picture": "生成图片"
 }
 if debug:
@@ -364,6 +368,10 @@ if debug:
         "show_history": "查看历史"
     })
 
+button2_labels = {
+    "generate_chat_record": "生成对话",
+    "download_chat_record": "下载对话内容到本地"
+}
 # 在同一行排列按钮
 with st.container():
     n_button = len(button_labels)
@@ -374,10 +382,10 @@ with st.container():
         clear_meta = st.button(button_labels["clear_meta"], key="clear_meta")
         if clear_meta:
             st.session_state["meta"] = {
-                "user_info": "",
-                "bot_info": "",
-                "bot_name": "",
-                "user_name": ""
+                "role_A_name": "",
+                "role_A_info": "",
+                "role_B_name": "",
+                "role_B_info": "",
             }
             st.rerun()
 
@@ -386,7 +394,16 @@ with st.container():
         if clear_history:
             init_session()
             st.rerun()
-    
+
+    # 图片风格选项
+    with button_key_to_col["select_style"]:
+        option = st.selectbox(
+            button_labels["select_style"],
+            ("写实", "二次元", "水彩", "复古", "低多边形"),
+            label_visibility="collapsed"
+        )
+        st.session_state["picture_style"] = option
+
     with button_key_to_col["gen_picture"]:
         gen_picture = st.button(button_labels["gen_picture"], key="gen_picture")
 
@@ -406,18 +423,32 @@ with st.container():
             if show_history:
                 print(f"history = {st.session_state['history']}")
 
+# 生成对话数据按钮
+with st.container():
+    n_button2 = len(button2_labels)
+    cols2 = st.columns(n_button2)
+    button2_key_to_col = dict(zip(button2_labels.keys(), cols2))
+    with button2_key_to_col["generate_chat_record"]:
+        generate_chat_record = st.button(
+            button2_labels["generate_chat_record"],
+            key="generate_chat_record",
+            icon=":material/mood:"
+        )
+
 
 # 展示对话历史
 for msg in st.session_state["history"]:
-    if msg["role"] == "user":
-        with st.chat_message(name="user", avatar="user"):
+    if msg["role"] == st.session_state["meta"]['role_A_name']:
+        with st.chat_message(name=st.session_state["meta"]['role_A_name']):
             st.markdown(msg["content"])
-    elif msg["role"] == "assistant":
-        with st.chat_message(name="assistant", avatar="assistant"):
+    elif msg["role"] == st.session_state["meta"]['role_B_name']:
+        with st.chat_message(name=st.session_state["meta"]['role_B_name']):
             st.markdown(msg["content"])
     elif msg["role"] == "image":
         with st.chat_message(name="assistant", avatar="assistant"):
             st.image(msg["image"], caption=msg.get("caption", None))
+    elif msg["role"] == "":
+        continue
     else:
         raise Exception("Invalid role")
 
@@ -426,39 +457,96 @@ if gen_picture:
     draw_new_image()
 
 
-with st.chat_message(name="user", avatar="user"):
-    input_placeholder = st.empty()
-with st.chat_message(name="assistant", avatar="assistant"):
-    message_placeholder = st.empty()
-
-
 def output_stream_response(response_stream: Iterator[str], placeholder):
     content = ""
     for content in itertools.accumulate(response_stream):
         placeholder.markdown(content)
     return content
 
+meta_role_A_as_user = {
+        'assistant_name': st.session_state["meta"]['role_B_name'],
+        'assistant_info': st.session_state["meta"]['role_B_info'],
+        'user_name': st.session_state["meta"]['role_A_name'],
+        'user_info': st.session_state["meta"]['role_A_info']
+    }
 
-def start_chat():
-    query = st.chat_input("开始对话吧")
-    if not query:
-        return
-    else:
-        if not verify_meta():
-            return
-        if not API_KEY:
-            st.error("未设置API_KEY")
+meta_role_B_as_user = {
+        'assistant_name': st.session_state["meta"]['role_A_name'],
+        'assistant_info': st.session_state["meta"]['role_A_info'],
+        'user_name': st.session_state["meta"]['role_B_name'],
+        'user_info': st.session_state["meta"]['role_B_info']
+    }
 
-        input_placeholder.markdown(query)
-        st.session_state["history"].append(TextMsg({"role": "user", "content": query}))
-        
-        response_stream = get_characterglm_response(filter_text_msg(st.session_state["history"]), meta=st.session_state["meta"])
-        bot_response = output_stream_response(response_stream, message_placeholder)
-        if not bot_response:
-            message_placeholder.markdown("生成出错")
-            st.session_state["history"].pop()
+'''
+处理prompt并请求GLM大模型：
+- 当轮到角色A回复时，在传给大模型的参数中，将角色A设为assistant, 将角色B设为user;
+- 当轮到角色B回复时，在传给大模型的参数中，将角色B设为assistant, 将角色A设为user;
+'''
+def start_chat(role_name, placeholder):
+    history_tmp = []
+    history = filter_text_msg(st.session_state["history"])
+
+    for m in history:
+        new_message = m.copy()
+        if new_message["role"] == role_name:
+            new_message["role"] = 'user'
         else:
-            st.session_state["history"].append(TextMsg({"role": "assistant", "content": bot_response}))
-            
-    
-start_chat()
+            new_message["role"] = 'assistant'
+        history_tmp.append(new_message)
+
+    if role_name == st.session_state["meta"]['role_A_name']:
+        meta = meta_role_A_as_user
+    else:
+        meta = meta_role_B_as_user
+
+    response_stream = get_characterglm_response(history_tmp, meta=meta)
+    bot_response = output_stream_response(response_stream, placeholder)
+    print(bot_response)
+    return bot_response
+
+
+# 交替生成对话数据并展示
+if generate_chat_record:
+    print(f"交替生成对话 = {st.session_state['meta']}")
+    # 无历史对话时，角色A开场
+    if len(filter_text_msg(st.session_state["history"])) < 1:
+        start_message = "开始对话吧"
+        st.session_state["history"].append({"role": st.session_state["meta"]['role_A_name'], "content": start_message})
+    # 生成多轮对话
+    for _ in range(10):
+        # 根据角色A的最后一次发言,角色B回复
+        with st.chat_message(name=st.session_state["meta"]['role_B_name']):
+            role_B_message_placeholder = st.empty()
+        role_B_response = start_chat(st.session_state["meta"]['role_A_name'], role_B_message_placeholder)
+        if not role_B_response:
+            st.markdown("生成出错")
+            break
+        else:
+            st.session_state["history"].append(TextMsg({"role": st.session_state["meta"]['role_B_name'], "content": role_B_response}))
+
+        # time.sleep(0.5)
+
+        # 根据角色B的最后一次发言,角色A回复
+        with st.chat_message(name=st.session_state["meta"]['role_A_name']):
+            role_A_message_placeholder = st.empty()
+        role_A_response = start_chat(st.session_state["meta"]['role_B_name'], role_A_message_placeholder)
+        if not role_A_response:
+            st.markdown("生成出错")
+            break
+        else:
+            st.session_state["history"].append(TextMsg({"role": st.session_state["meta"]['role_A_name'], "content": role_A_response}))
+
+
+# 保存对话数据按钮
+if len(filter_text_msg(st.session_state["history"])) > 0:
+    with st.container():
+        with button2_key_to_col["download_chat_record"]:
+            download_chat_record = st.download_button(
+                label=button2_labels["download_chat_record"],
+                key="download_chat_record",
+                data=('\n'.join([f'{msg["role"]}: {msg["content"]}' for msg in filter_text_msg(st.session_state['history'])])),
+                file_name='chat_history.txt',  # 下载文件的名称
+                mime='text/plain', # MIME 类型
+                icon=":material/book:",
+                type="secondary"
+            )
